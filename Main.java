2842a0f063742da53d2d2eb6fcbfcf40ab0cb964
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-03-17 23:06:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+	public static void main(String[]args){
+		//initialization"int =number1",condition,loop body"number<=10;",update"number++""
+		//start with 1 and it will be true since 1 is <=10,then it will update and continue untill it's not true/reach 10. 
+		for(int number=1; number <=10; number++){
+
+			System.out.println(number);
+		}
+	}
+}
+		
